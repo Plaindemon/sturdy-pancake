f27edba30d1e440e77500c25a9b614955d9a80c6
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-26 00:17:39 -0600

    var added to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
+//
+
+
+// variables created for the Weather card/container
+var date = document.getElementById('date');
+var temperature = document.getElementById('temp');
+var wind = document.getElementById('wind');
+var humidity = document.getElementById('humid');
+var uvIndex = document.getElementById('uv-index');
 // citysearch used to grab the search button element
 var citySearch = document.getElementById("city-search-btn");
 
@@ -5,4 +14,8 @@ var searchBtn = function() {
     console.log("bhhollo");
 };
 
-searchBtn();
+// searchBtn();
+
+// var getCity = function() {
+//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={9f34bc786ad6188d153dff67826d4506}");
+// };
