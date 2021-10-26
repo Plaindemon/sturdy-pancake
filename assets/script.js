925e0ee926fd246adf4e923d5717646182dd8aca
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-25 18:10:43 -0600

    more html added and css styling

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// citysearch used to grab the search button element
+var citySearch = document.getElementById("city-search-btn");
+
+var searchBtn = function() {
+    console.log("bhhollo");
+};
+
+searchBtn();
