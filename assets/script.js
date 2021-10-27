9fe844296cffa70f0db36b88b4af0c215b47f182
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-10-26 18:38:00 -0600

    added variables to link js to the parts of HTML

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,26 @@ var humidity = document.getElementById('humid');
 var uvIndex = document.getElementById('uv-index');
 // citysearch used to grab the search button element
 var citySearch = document.getElementById("city-search-btn");
+//
+var cityName = document.getElementById('city-name');
+var pastCity = document.getElementById('past-city')
+// variable to create the search form input
+// var searchTextBox = document.createElement("p");
+// searchTextBox.textContent = "Ohio";
+// searchTextBox;
 
 var searchBtn = function() {
-    console.log("bhhollo");
+    
+    // when the city search button is clicked it creates an alert
+    citySearch.addEventListener('click', function(){
+        alert("button clicked");
+    });
+    console.log(citySearch);
+    //
+
 };
 
-// searchBtn();
+searchBtn();
 
 // var getCity = function() {
 //     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={9f34bc786ad6188d153dff67826d4506}");
