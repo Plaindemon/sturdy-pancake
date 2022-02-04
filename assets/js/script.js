896f2d1ef99f238102c6d0ebc93276b0eb138f01
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-03 22:27:14 -0700

    added date

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,43 @@ var citySearch =
 var weatherContainerEl = document.querySelector("#weather-container");
 
 
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
+// let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+// console.log(searchHistory);
+var searches = [];
+var cities = document.getElementById("search").value;
+// create function to get the submission from local storage
+function storeSearch() {
+    // get the local storage
+    var getLocal = JSON.parse(localStorage.getItem("search"));
+    // gets the value of the city from the input field - cities
 
+    // puts current city search into searches array 
+    var current = { "searches": cities };
+    // pushes searches
+    searches.push(getLocal);
+    console.log(searches)
+    //pushed back up to local storage
+    searches.push(current)
+    localStorage.setItem('searches', JSON.stringify(searches));
+}
 
+// display cities in HTML after the new city is submitted
+function searchDisplay() {
+    var listDiv = document
+        .getElementById('past-city');
+    for (let i = 0; i < searches.length; i++) {
+        listDiv.textContent = "History List" + searches[i].cities;
+    }
 
+}
+
+console.log(searchDisplay())
+searchDisplay();
 
 function initSearch(lat, long) {
     var key = 'd1421d1863129a775873c0c766a5d5c6';
 
-    const base = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + '&lon=' + long + '&appid=' + key+'&units=imperial';
+    const base = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + '&lon=' + long + '&appid=' + key + '&units=imperial';
     // const base = `https://api.openweathermap.org/data/2.5/onecall?lat=$(lat)&lon=$(lon)&appid=$(key)`;
     console.log(base);
     fetch(base)
@@ -27,19 +54,22 @@ function initSearch(lat, long) {
         })
         .then(data => {
             console.log(data)
+            // get date 
+            const dateTime = new Date();
+            document.getElementById("date").textContent = dateTime;
             document.getElementById('description').textContent = data.current.weather[0].description;
             document.getElementById('wind').innerHTML = data.current.wind_speed;
             document.getElementById('humid').innerHTML = data.current.humidity;
-            document.getElementById('weather-icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`)
+            // document.getElementById('weather-icon').setAttribute('src', `https://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`);
             document.getElementById('temp').innerHTML = data.current.temp + '&deg;';
             document.getElementById('uvi').innerHTML = data.current.uvi;
-             var htmlContent = "";
-             for(let i=1; i< data.daily.length; i++) {
-                 htmlContent+=` <div id="${i}" class="container card-body">
+            var htmlContent = "";
+            for (let i = 1; i < data.daily.length; i++) {
+                htmlContent += ` <div id="${i}" class="container card-body">
 
-                 <h4 class="card-title date" class="date"><strong>Day${i}</strong>
-                 <img src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png">
-                 </h4>
+                 <h4 class="card-title date" class="date"><strong>Day: ${dateTime}</strong></h4>
+                 <img  class="img" src="https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png">
+                 
                  <p class="card-text temp-card" class="temp">Temperature: ${data.daily[i].temp.day}<p>
                  <p class="card-text wind-card" class="wind">Wind Speed: ${data.daily[i].wind_speed}</p>
                  <p class="card-text humid-card" class="humid">Humidity Level: ${data.daily[i].humidity} </p>
